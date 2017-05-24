1377d523fc08e69fab81a3c82133c8af5434322a
Guest User <Guest@Epicodus-6G.local>
2017-05-24 13:57:17 -0700

    pass user input data

--- app/src/main/java/com/example/guest/boggle/ResultActivity.java
+++ app/src/main/java/com/example/guest/boggle/ResultActivity.java
@@ -1,14 +1,29 @@
 package com.example.guest.boggle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class ResultActivity extends AppCompatActivity {
+
+    @Bind(R.id.userWordTextView)
+    TextView mUserWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ButterKnife.bind(this);
+
+
+        Intent intent = getIntent();
+        String userInput = intent.getStringExtra("userInput");
+
+        mUserWord.setText(userInput);
     }
 
 
--- app/src/main/java/com/example/guest/boggle/SubmitActivity.java
+++ app/src/main/java/com/example/guest/boggle/SubmitActivity.java
@@ -1,8 +1,12 @@
 package com.example.guest.boggle;
 
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import butterknife.Bind;
@@ -10,9 +14,11 @@ import butterknife.ButterKnife;
 
 
 
-public class SubmitActivity extends AppCompatActivity {
+public class SubmitActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Bind(R.id.randomStringTextView) TextView mRandomString;
+    @Bind(R.id.userInputEditText) EditText mUserInput;
+    @Bind(R.id.submitButton) Button mSubmitButton;
 
     BoggleService service1 = new BoggleService("","");
 
@@ -22,8 +28,17 @@ public class SubmitActivity extends AppCompatActivity {
         setContentView(R.layout.activity_submit);
         ButterKnife.bind(this);
 
+        mSubmitButton.setOnClickListener(this);
         mRandomString.setText(service1.textGenerator());
 
-
     }
+
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(SubmitActivity.this, ResultActivity.class);
+        String userInput = mUserInput.getText().toString();
+        intent.putExtra("userInput", userInput);
+        startActivity(intent);
+    }
+
 }
