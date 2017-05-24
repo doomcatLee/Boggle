8f90b36a0bd72847d89bae11369056c3312ac45e
Guest User <Guest@Epicodus-6G.local>
2017-05-24 10:36:23 -0700

    add relativelayout instead of constraint layout

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.guest.boggle;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.playButton) Button mPlayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mPlayButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if (v == mPlayButton){
+            Intent intent = new Intent(MainActivity.this, SubmitActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
 }
